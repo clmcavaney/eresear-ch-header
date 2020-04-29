a6fa055425a1f0a668a07c5af7386b358bbee72b
Christopher McAvaney <christopher.mcavaney@gmail.com>
2020-04-29 13:11:57 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Simple plugin to modify the header of YOURLS pages.
+
+Based on https://github.com/YOURLS/plugin-sample
+
+Specifically developed for the https://eresear.ch/ website.
+Only seen if you are an administrator logging into the YOURLS instance.
---
+++ plugin.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: eresear.ch header
+Plugin URI: <TBC>
+Description: This plugin modifies the header of YOURLS, specifically for the eResearch shorten URL service (https://eresear.ch)
+Version: 1.0
+Author: Christopher McAvaney
+Author URI: http://deakinresear.ch/eresearch
+*/
+
+// No direct call
+if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
+yourls_add_action( 'html_logo', 'er_header');
+
+function er_header() {
+?>
+<h2>Servicing the eResearch community</h2>
+<?php
+	if ( isset($_SESSION['attributes']['edupersonorcid']) ) {
+?>
+		<p><?php echo $_SESSION['attributes']['displayname'];?>
+<?php
+		if ( isset($_SESSION['attributes']['edupersonorcid']) && $_SESSION['attributes']['edupersonorcid'] != '' ) {
+?>
+ &ndash; <img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" style="vertical-align: text-bottom;" /> <a href="<?php echo $_SESSION['attributes']['edupersonorcid'];?>"><?php echo $_SESSION['attributes']['edupersonorcid']; ?></a></p>
+<?php
+		}
+?>
+<?php
+	}
+}
+?>
